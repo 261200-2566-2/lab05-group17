1a0447099d94b376f740747fb0171a1855231b2a
Delphisss <Kong_led_@hotmail.com>
2023-12-05 13:58:36 +0700

    job

---
+++ src/Character.java
@@ -0,0 +1,193 @@
+class Character {
+    private String name;
+    private String gender;
+    private String job;
+    private int level;
+    private int hp;
+    private int mana;
+    private int str;
+    private int agi;
+    private int shield; 
+    private int runSpeed = 34;
+    private int damage ;
+    private int totalWeight = 0; ;
+    private Equipment[] weapon = new Equipment[2];
+    private Equipment[] accessory = new Equipment[2];
+    
+    public Character(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+
+    }
+
+    public void setJobStatus(String job) {
+        this.job = job;
+        if ("Novice".equals(job)) {
+            this.level = 1;
+            this.hp = 40;
+            this.mana = 20;
+            this.str = 1;
+            this.agi = 1;
+            this.shield = 1;
+        } else if ("Swordman".equals(job)) {
+            this.level = 1;
+            this.hp = 80;
+            this.mana = 40;
+            this.str = 5;
+            this.agi = 2;
+            this.shield = 1;
+            
+        } else if ("Mage".equals(job)) {
+            this.level = 1;
+            this.hp = 50;
+            this.mana = 100;
+            this.str = 0;
+            this.agi = 0;
+            this.shield = 1; 
+        }
+    }
+
+    // max on lv.99
+    void levelUp() {
+        if(level <= 99){
+            level++;
+            hp = 40 + 5*level;
+            mana = 20 + 5*level;
+            str = 1 + 5*level;
+            agi = 34 + 2*level;
+            if(agi >= 100){
+                agi = 100;
+            }
+        }
+    }
+
+    String getName() {return name;}
+    String getGender() {return gender;}
+    String getJob() {return job;}
+    int getLevel() {return level;}
+    int getHp() {return hp;}
+    int getMana() {return mana;}
+    int getStr() {return str;}
+    double getAgi() {return agi;}
+
+    Equipment[] getWeapons() {return weapon;}
+
+    public void setDamage(){
+        if(weapon[0] != null){
+            damage += weapon[0].getDamage();
+        }
+    }
+    public void setShield(){
+        if(weapon[1] != null){
+            shield += weapon[1].getReduceDamage();
+        }
+    }
+
+    public void setRunSpeed() {
+        
+        runSpeed = 34;
+    
+        if (weapon[0] != null) {
+            totalWeight += weapon[0].getWeight();
+        }
+    
+        if (weapon[1] != null) {
+            totalWeight += weapon[1].getWeight();
+        }
+    
+        if (totalWeight > 150 && totalWeight <= 200) {
+            runSpeed *= 0.75; // วิ่งช้าลง 25%
+        } else if (totalWeight > 200 && totalWeight <= 250) {
+            runSpeed *= 0.7; // วิ่งช้าลง 30%
+        } else if (totalWeight > 250) {
+            runSpeed *= 0.5; // วิ่งช้าลง 50%
+        }
+    }
+
+    // ระบบอัพเลเวล ยังไม่ได้ใช้
+    // public void setJob(String newJob) {
+    //     if(level == 10){
+    //         job = newJob;
+    //     }
+    // }
+    
+    public void equipWeapon(Equipment weapon1) {
+        if (weapon1.getType().equals("sword")||weapon1.getType().equals("dagger")||weapon1.getType().equals("wand")) { 
+            weapon[0] = weapon1;
+            setRunSpeed();
+            setDamage();
+        } else if (weapon1.getType().equals("shield")) {
+            weapon[1] = weapon1;
+            setRunSpeed();
+            setShield();
+        }
+    }
+
+    public void equipWeapon(Equipment weapon1, Equipment weapon2) {
+        weapon[0] = weapon1;
+        weapon[1] = weapon2;
+        setRunSpeed();
+        setDamage();
+        setShield();
+    }
+
+    public void equipAccessory(Equipment accessory1, Equipment accessory2) {
+        accessory[0] = accessory1;
+        accessory[1] = accessory2;
+        applyAccessoryEffects();
+    }
+
+    private void applyAccessoryEffects() {
+        for (Equipment acc : accessory) {
+            if (acc != null) {
+                hp += acc.getBonusHp();
+                agi += acc.getBonusSpeed();
+                mana += acc.getBonusMana();
+            }
+        }
+    }
+
+    public void displayAccessoryInfo() {
+        System.out.println("=== Accessory Info ===");
+        for (Equipment acc : accessory) {
+            if (acc != null) {
+                acc.getWeaponInfo();
+            } else {
+                System.out.println("No equipped.");
+            }
+        }
+        System.out.println("======================");
+    }
+
+    public void displayCharacterInfo() {
+        System.out.println("=== Character Info ===");
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Job: " + job);
+        System.out.println("Level: " + level);
+        System.out.println("HP: " + hp);
+        System.out.println("Mana: " + mana);
+        System.out.println("ASPD: " + agi*2);
+        System.out.println("Run Speed: " + runSpeed);
+        System.out.println("Damage: " + damage);
+        System.out.println("Shield: " + shield);
+        System.out.println("Weight: " + totalWeight);
+        System.out.println("=== Weapon Info ===");
+    
+        for (Equipment w : weapon ) {
+            if (w != null) {
+                w.getWeaponInfo();
+            } else {
+                System.out.println("No equipped.");
+            }
+        }
+    }
+    
+
+    void displayInfo() {
+        displayCharacterInfo();
+        displayAccessoryInfo();
+    }
+
+    
+}
---
+++ src/Equipment.java
@@ -0,0 +1,102 @@
+class Equipment {
+    private String name;
+    private String type;
+    private int level;
+    private int reducesDamage;
+    private int damage;
+    private int weight;
+    private int bonusHp;
+    private int bonusSpeed;
+    private int bonusMana;
+
+    public static Equipment W001 = new Equipment("Sword", "Wooden Sword", 1, 10, 20, 0, 0, 0);
+    public static Equipment W002 = new Equipment("Sword", "Iron Sword", 1, 15, 25, 0, 0, 0);
+    public static Equipment W003 = new Equipment("Dagger", "Obsidian Dagger", 1, 69, 40, 0, 0, 0);
+    public static Equipment W004 = new Equipment("Wand", "Evil Bone Wand", 1, 70, 50, 0, 0, 50);
+    
+    public static Equipment S001 = new Equipment("Shield", "Wooden Shield", 1, 5, 15, 0, 0, 0);
+    public static Equipment S002 = new Equipment("Shield", "Iron Shield", 1, 10, 20, 0, 0, 0);
+    public static Equipment S003 = new Equipment("Shield", "Book of OOP", 1, 10, 50, 0, 0, 0);
+    
+    public static Equipment A001 = new Equipment("Accessory", "Ring of Strength", 1, 0, 1, 50, 0, 0);
+    public static Equipment A002 = new Equipment("Accessory", "Ring of Speed", 1, 0, 1, 0, 20, 0);
+    public static Equipment A003 = new Equipment("Accessory", "Ring of Mana", 1, 0, 1, 0, 0, 100);
+    public static Equipment A004 = new Equipment("Accessory", "Ring of one of all", 1, 0, 1, 100, 20, 50);    
+
+    public Equipment(String type, String name, int level, int dmgOrRdmg, int weight, int bonusHp, int bonusSpeed, int bonusMana) {
+        this.name = name;
+        this.level = level;
+        this.type = type.toLowerCase();
+        
+        if(this.type.equals("sword")){
+            this.damage = dmgOrRdmg * (2 + level);
+        }
+        if(this.type.equals("dagger")){
+            this.damage = dmgOrRdmg * (2 + level);
+        }
+        if(this.type.equals("wand")){
+            this.damage = dmgOrRdmg * (2 + level);
+        }
+        if(this.type.equals("shield")){
+            this.reducesDamage = dmgOrRdmg + (3 * level);
+        }
+        this.weight = weight;
+        this.bonusHp = bonusHp;
+        this.bonusSpeed = bonusSpeed;
+        this.bonusMana = bonusMana;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String  getType(){
+        return type;
+    }
+
+    public int getWeight(){
+        return weight; 
+    }
+    public int getDamage(){
+        return damage; 
+    }
+    public double getReduceDamage(){
+        return reducesDamage; 
+    }
+
+    public int getBonusHp() {
+        return bonusHp;
+    }
+    
+    public int getBonusSpeed() {
+        return bonusSpeed;
+    }
+    
+    public int getBonusMana() {
+        return bonusMana;
+    }
+
+
+    public void getWeaponInfo(){
+        if("sword".equals(type)){
+            System.out.println("Name: " + name + "\tLevel: " + level + "\tDamage: " 
+            + damage + "\tWeight: " + weight);
+        }
+        if("shield".equals(type)){
+            System.out.println("Name: " + name + "\tLevel: " + level + "\tReduces Damage: " 
+            + reducesDamage + "\tWeight: " + weight);
+        }
+        if("dagger".equals(type)){
+            System.out.println("Name: " + name + "\tLevel: " + level + "\tDamage: " 
+            + damage + "\tWeight: " + weight + "\tBounsHp: " + bonusHp + "\tBounsSpeed: " + bonusSpeed + "\tBonseMana: " + bonusMana);
+        }
+        if("wand".equals(type)){
+            System.out.println("Name: " + name + "\tLevel: " + level + "\tDamage: " 
+            + damage + "\tWeight: " + weight + "\tBounsHp: " + bonusHp + "\tBounsSpeed: " + bonusSpeed + "\tBonseMana: " + bonusMana);
+        }
+        if("accessory".equals(type)){
+            System.out.println("Name: " + name + "\tLevel: " + level + "\tDamage: " 
+            + damage + "\tWeight: " + weight + "\tBounsHp: " + bonusHp + "\tBounsSpeed: " + bonusSpeed + "\tBonseMana: " + bonusMana);
+        }
+    }
+}
