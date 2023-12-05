fdae051c6dd6825a74eb465c17c549d595eea255
asporQ <120199524+asporQ@users.noreply.github.com>
2023-12-05 19:04:27 +0700

    okieokie

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     Character player1 = new Character("Somchai", "Female", "Novice");
     Character player2 = new Character("Sommark", "Male", "Novice");
     
-    //player1.equipWeapon(Equipment.W001);
+    player1.equipWeapon(Equipment.W001);
     player1.equipAccessory(Equipment.A001, null);
     player2.equipWeapon(Equipment.W002);
-    // player2.equipAccessory(Equipment.A003, null);
+    player2.equipAccessory(Equipment.A003, null);
 
     Action.Attack attack = new Action.Attack(player2, player1);
     Action.Defend defend = new Action.Defend(player1);
